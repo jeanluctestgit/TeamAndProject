0d593bb69bfb8cb1c893fe0d8824cbe4a7c8dbed
jeanluctestgit <jeanluc.deshayes92@gmail.com>
2021-03-05 17:00:18 +0100

    maj doc management - upload

--- teamaproject/src/components/GED/doc_management.component.js
+++ teamaproject/src/components/GED/doc_management.component.js
@@ -5,15 +5,25 @@ import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 export default class DocManagement extends Component {
 
+    constructor(props) {
+        super(props);
+        this.handleUpload = this.handleUpload.bind(this);
+    }
+
     state = {
-       documents : [
-           {
-               name : 'myPdf.pdf' , path : "http://localhost:3000/Mon%20SQL.pdf"
-           },
-           {
-            name : 'myCSV.csv' , path : "http://localhost:3000/course_order_line.csv"
-           }
-       ]
+        documents: [
+            {
+                name: 'myPdf.pdf', path: "http://localhost:3000/Mon%20SQL.pdf"
+            },
+            {
+                name: 'myCSV.csv', path: "http://localhost:3000/course_order_line.csv"
+            }
+        ]
+    }
+
+    handleUpload = (e) => {
+            e.preventDefault();
+            
     }
 
     getDownloadFile = async (path) => {
@@ -26,20 +36,32 @@ export default class DocManagement extends Component {
         return (
             <div>
                 Documents Of Project
+                <div className="card">
+                    <form onSubmit={this.handleUpload}>
+                        <div>
+                            <label>File to upload</label>
+                            <input type = "file" name = "file" id = "file"/>
+                        </div>
+                        <div>
+                            <input type="submit" value="upload"/>
+                        </div>
+                    </form>
+                </div>
+
                 <ListGroup>
-                  {
-                      this.state.documents.map((doc , index )=> {
-                          return (
-                              <ListGroupItem key = {index}>
-                                {doc.name}
-                                <DownloadLink label="Download" className = "btn btn-primary float-right" style={{color:'white'}}
-                                  filename={doc.name} exportFile={() => this.getDownloadFile(doc.path)} />
-                              </ListGroupItem>
-                          )
-                      })
-                  }
+                    {
+                        this.state.documents.map((doc, index) => {
+                            return (
+                                <ListGroupItem key={index}>
+                                    {doc.name}
+                                    <DownloadLink label="Download" className="btn btn-primary float-right" style={{ color: 'white' }}
+                                        filename={doc.name} exportFile={() => this.getDownloadFile(doc.path)} />
+                                </ListGroupItem>
+                            )
+                        })
+                    }
                 </ListGroup>
-                
+
             </div>
         )
     }
